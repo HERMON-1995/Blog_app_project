a94a9dc8099ea074d2437d55357127d7f7e6190f
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-06 15:35:49 +0300

    Create and run post migration file

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,15 @@
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_122022) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_122914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "title"
+    t.text "text"
+    t.integer "comment_counter", default: 0
+    t.integer "likes_counter", default: 0
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -9,10 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_122022) do
     t.integer "post_counter", default: 0
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
 end
-
-
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
