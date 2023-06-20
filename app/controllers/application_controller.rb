3ee81abef8cb8246c659b8ca9bcb4a649436d4c3
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-20 21:44:57 +0100

    Add before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # def current_user
   #   @current_user ||= User.first
   # end
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+#   protected
+
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+#   end
 end
