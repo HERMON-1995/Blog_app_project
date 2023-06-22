af6e700bc9bc3db10997d40d3f4a82ba300f7a99
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-22 23:24:50 +0300

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
+
   devise_for :users
+
   # get 'comments/index'
   # get 'likes/index'
   
