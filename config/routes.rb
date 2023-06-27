a62ecbf5ec625e740d829344c5f6b9cae50b2690
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-27 12:15:53 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
 
   namespace :api do
     namespace :v1 do
       resources :users, only: [] do
-        resources :posts, only: [:index] do
-          resources :comments, only: [:index, :create]
+        resources :posts, only: [:index, :create, :update, :destroy] do
+          resources :comments, only: [:index, :create, :update, :destroy]
         end
       end
     end
